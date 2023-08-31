a2352767ecfc79bb7e2820863a37aa56726d0873
elf <Nyy2@163.com>
2023-08-31 23:10:44 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'feb1ffcc-4980-405d-a33a-c7ebefe2571f';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
